f60b1abc365b96caffeb74f472636677b6ea0f68
TakesxiSximada <sximada@gmail.com>
2024-07-28 21:43:38 +0900

    add default module

---
+++ mods/default/init.lua
@@ -0,0 +1,4 @@
+local mod_path = minetest.get_modpath(minetest.get_current_modname())
+
+dofile(mod_path .. "/minimal.lua")
+dofile(mod_path .. "/command.lua")
---
+++ mods/default/minimal.lua
@@ -0,0 +1,150 @@
+local S = minetest.get_translator(minetest.get_current_modname())
+
+default = {}
+
+-- sound
+function default.node_sound_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name = "", gain = 1.0}
+	table.dug = table.dug or
+			{name = "default_dug_node", gain = 0.25}
+	table.place = table.place or
+			{name = "default_place_node_hard", gain = 1.0}
+	return table
+end
+
+function default.node_sound_stone_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name = "default_hard_footstep", gain = 0.2}
+	table.dug = table.dug or
+			{name = "default_hard_footstep", gain = 1.0}
+	default.node_sound_defaults(table)
+	return table
+end
+
+function default.node_sound_water_defaults(table)
+	table = table or {}
+	table.footstep = table.footstep or
+			{name = "default_water_footstep", gain = 0.2}
+	default.node_sound_defaults(table)
+	return table
+end
+
+
+-- stone
+minetest.register_node("default:stone", {
+	description = S("Stone"),
+	tiles = {"default_stone.png"},
+	groups = {cracky = 3, stone = 1},
+	drop = "default:cobble",
+	legacy_mineral = true,
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("default:cobble", {
+	description = S("Cobblestone"),
+	tiles = {"default_cobble.png"},
+	is_ground_content = false,
+	groups = {cracky = 3, stone = 2},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+-- Liquids
+minetest.register_node("default:water_source", {
+	description = S("Water Source"),
+	drawtype = "liquid",
+	waving = 3,
+	tiles = {
+		{
+			name = "default_water_source_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+		{
+			name = "default_water_source_animated.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "default:water_flowing",
+	liquid_alternative_source = "default:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {water = 3, liquid = 3, cools_lava = 1},
+	sounds = default.node_sound_water_defaults(),
+})
+
+minetest.register_node("default:water_flowing", {
+	description = S("Flowing Water"),
+	drawtype = "flowingliquid",
+	waving = 3,
+	tiles = {"default_water.png"},
+	special_tiles = {
+		{
+			name = "default_water_flowing_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.5,
+			},
+		},
+		{
+			name = "default_water_flowing_animated.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.5,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "default:water_flowing",
+	liquid_alternative_source = "default:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {water = 3, liquid = 3, not_in_creative_inventory = 1,
+		cools_lava = 1},
+	sounds = default.node_sound_water_defaults(),
+})
+
+
+-- mapdisgens
+minetest.register_alias("mapgen_stone", "default:stone")
+minetest.register_alias("mapgen_water_source", "default:water_source")
